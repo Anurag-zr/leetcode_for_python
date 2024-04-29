4271e9550376b52d12bdc3864dc09302dbfdef66
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2024-04-29 13:41:42 +0530

    Time: 35 ms (70.58%) | Memory: 16.5 MB (58.02%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,14 +1,35 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        if(numRows == 0 ): return [[]]
-        if(numRows ==1 ): return [[1]]
+        # using recursion method
+        # if(numRows == 0 ): return [[]]
+        # if(numRows ==1 ): return [[1]]
 
-        prevRows = self.generate(numRows-1)
-        curRow = [1]*numRows
+        # prevRows = self.generate(numRows-1)
+        # curRow = [1]*numRows
 
-        for i in range(1,numRows-1):
-            curRow[i] = prevRows[-1][i-1] + prevRows[-1][i]
+        # for i in range(1,numRows-1):
+        #     curRow[i] = prevRows[-1][i-1] + prevRows[-1][i]
 
-        prevRows.append(curRow)
-        return prevRows
+        # prevRows.append(curRow)
+        # return prevRows
         
+
+        # using combinatorial formula
+
+        if numRows == 0 : return [[]]
+        if numRows ==1 : return [[1]]
+
+        ans = []
+        ans.append([1])
+        for i in range (1,numRows): 
+            prevRow = ans[-1]
+            curRow = [1]*(i+1)
+
+            for j in range(1,i):
+                curRow[j] = prevRow[j-1] + prevRow[j]
+
+            ans.append(curRow)
+
+        return ans
+
+        return ans
