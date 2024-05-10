49516d2962d71942614de2de3f81b626743160ab
Anurag-zr <56393428+Anurag-zr@users.noreply.github.com>
2024-05-10 13:26:41 +0530

    Time: 610 ms (63.05%) | Memory: 16.9 MB (78.98%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
@@ -1,14 +1,34 @@
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        heap = []
+        # brute force
+        # heap = []
 
-        for i in range(len(arr)-1):
-            for j in range(i+1,len(arr)):
-                fraction = arr[i]/arr[j]
-                heapq.heappush(heap,(fraction,(arr[i],arr[j])))
+        # for i in range(len(arr)-1):
+        #     for j in range(i+1,len(arr)):
+        #         fraction = arr[i]/arr[j]
+        #         heapq.heappush(heap,(fraction,(arr[i],arr[j])))
 
         
-        for _ in range(k):
-            a,b = heapq.heappop(heap)[1]
+        # for _ in range(k):
+        #     a,b = heapq.heappop(heap)[1]
 
-        return [a,b]
+        # return [a,b]
+
+        # optimized T.C
+
+        minHeap =[]
+
+        for i in range(len(arr)-1):
+            fraction = arr[i]/arr[-1]
+            heapq.heappush(minHeap,(fraction,(i,len(arr)-1)))
+        
+        for _ in range(k-1):
+            numInd,demInd = heapq.heappop(minHeap)[1]
+
+            if(demInd-1>numInd):
+                fraction = arr[numInd]/arr[demInd-1]
+                heapq.heappush(minHeap,(fraction,(numInd,demInd-1)))
+        
+
+        numInd,demInd = heapq.heappop(minHeap)[1]
+        return [arr[numInd],arr[demInd]]
